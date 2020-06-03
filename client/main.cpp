850fbc549f97127e821d1e857be6d61dec7779bf
Thuan Nguyen <hungthuanmk@Thuans-MacBook-Pro.local>
2020-06-03 15:54:07 +0700

    Add packet header

--- client/main.cpp
+++ client/main.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+#define SERVER_HOST = "localhost";
+#define SERVER_PORT = 1998;
+
+#define MAX_MSG_LEN = 1024;
+
 void main() {
     // Client main code here
 }
---
+++ common/constants.h
@@ -0,0 +1,9 @@
+#ifndef IMPORTED_CONSTANTS_H
+    #define IMPORTED_CONSTANTS_H
+
+    #define SERVER_HOST "localhost"
+    #define SERVER_PORT 1998
+
+    #define MAX_MSG_LEN 1024
+    #define MAX_USERNAME_LEN 100
+#endif
